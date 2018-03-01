a4fc81dfbcf7870860832dac3c4d2d57a3caf00f
22320 <2232029525@qq.com>
2018-03-01 16:20:31 +0800

    Initial commit

---
+++ app/src/main/java/www/yckj/com/baseslideclosedemo/NextActivity.java
@@ -0,0 +1,12 @@
+package www.yckj.com.baseslideclosedemo;
+
+import android.os.Bundle;
+
+public class NextActivity extends BaseSlideCloseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_next);
+    }
+}
